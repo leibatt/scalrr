7d98046958bcc9fe78cfac595dee3c2a6fa75500
Leilani Battle <leibatt@gmail.com>
2013-01-31 20:41:22 -0800

    adding hack to scatterplot file

--- static/js/QVisScatterPlot.js
+++ static/js/QVisScatterPlot.js
@@ -34,7 +34,7 @@ QVis.ScatterPlot.prototype.render = function(_data, _labels,_types, opts) {
 	var xscale = this.createScale(_data,_types,self.labelsfrombase.x_label,this.w,this.px,this.inv[0],false);
 	var yscale = this.createScale(_data,_types,self.labelsfrombase.y_label,this.h,this.py,this.inv[1],false);
 	var zscale = this.createScale(_data,_types,self.labelsfrombase.z_label,0,0,this.inv[2],true)
-			.range(colorbrewer.Oranges[9]);
+			.range(colorbrewer[this.colorscheme][9]);
 	if((self.labelsfrombase.z_label in this.min) && (self.labelsfrombase.z_label in this.max)) {
 		if(this.inv[2]){
 			zscale.domain([this.max[self.labelsfrombase.z_label],this.min[self.labelsfrombase.z_label]]);
@@ -49,7 +49,7 @@ QVis.ScatterPlot.prototype.render = function(_data, _labels,_types, opts) {
 		.attr('width', this.w)
 		.attr('height', this.h)
 		.attr('class', 'g')
-		.attr('id', 'svg_'+this.rootid);
+		.attr('id', 'svg1_'+this.rootid);
 	this.add_axes(xscale, yscale,self.labelsfrombase.x_label,self.labelsfrombase.y_label, self.strings,_types);
 				
 	this.circlecontainer = this.svg.append('g')
@@ -63,6 +63,6 @@ QVis.ScatterPlot.prototype.render = function(_data, _labels,_types, opts) {
 	//just testing the rects function
 	//this.drawRects(this.circlecontainer,_data,_types,xscale,yscale,self.labelsfrombase.x_label,self.labelsfrombase.y_label,this.defaultRadius,this.defaultRadius,this.defaultColor);
 
-	this.add_brush(xscale,yscale,self.labelsfrombase.x_label,self.labelsfrombase.y_label,function(d) {return zscale(d[self.labelsfrombase.z_label]);},this.circlecontainer);
+	//this.add_brush(xscale,yscale,self.labelsfrombase.x_label,self.labelsfrombase.y_label,function(d) {return zscale(d[self.labelsfrombase.z_label]);},this.circlecontainer);
 
 }
